504b5f1119d759b77d74fc36969085fb9dad7c16
alamin20cse <md.alamin165086@gmail.com>
2024-09-05 23:01:03 +0600

    function perametar

--- function.js
+++ function.js
@@ -1,10 +1,19 @@
 
-function sum()
+function sums()
 {
     var a=10;
     var b=20;
     var s=a+b;
     console.log(s);
 }
-sum();
-sum();
+sums();
+
+
+function sum(a,b)
+{
+    
+    var s=a+b;
+    console.log(s);
+}
+sum(2,3);
+sum(5,5);
