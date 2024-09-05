71f4df72ee31154ca9e4166afd9329324f374b21
alamin20cse <md.alamin165086@gmail.com>
2024-09-05 23:15:40 +0600

    return

--- function.js
+++ function.js
@@ -1,19 +1,45 @@
 
+// function sums()
+// {
+//     var a=10;
+//     var b=20;
+//     var s=a+b;
+//     console.log(s);
+// }
+// sums();
+
+
+// function sum(a,b)
+// {
+    
+//     var s=a+b;
+//     console.log(s);
+// }
+// sum(2,3);
+// sum(5,5);
+
+
+
+
+
 function sums()
 {
     var a=10;
     var b=20;
     var s=a+b;
-    console.log(s);
+   return s;
 }
-sums();
+var op=sums();
+console.log(op);
 
 
 function sum(a,b)
 {
     
     var s=a+b;
-    console.log(s);
+   return s;
 }
-sum(2,3);
-sum(5,5);
+var op2= sum(2,3);
+console.log(op2);
+var op3= sum(5,5);
+console.log(op3);
