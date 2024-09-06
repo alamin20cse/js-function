6c417946352852a08c5726ee806e09d9aebe759b
alamin20cse <md.alamin165086@gmail.com>
2024-09-06 23:04:57 +0600

    moduel 22 complet

--- function.js
+++ function.js
@@ -57,14 +57,119 @@
 
 
 
-function numbertow(inputarr)
-{
+// function numbertow(inputarr)
+// {
    
-    for(var i of inputarr)
-    {
-       console.log(i);
+//     for(var i of inputarr)
+//     {
+//        console.log(i);
 
-    }
+//     }
   
-}
-numbertow([5,10,3]);
+// }
+// numbertow([5,10,3]);
+
+
+
+// for(let i=0;i<10;i++)
+// {
+//     console.log(i);
+//     if(i==5)
+//     {
+//         break;
+//     }
+// }
+
+
+/// ince to feet
+
+// function incetofeet(number)
+// {
+//     let x =parseInt(number/12);
+//     let y=number%12;
+
+//     console.log(x+" FEET " +y+" INCE");
+// }
+
+// incetofeet(62);
+// incetofeet(75);
+
+
+
+
+
+
+
+// odd avarege;
+
+
+// function oddAvg(arr)
+// {
+//     let cnt=0,sum=0;
+    
+//     for(let i of arr)
+//     {
+//         // cnt++;
+//         if(i%2===1)
+//         {
+//             sum=sum+i;
+//             cnt++;
+//         }
+//     }
+
+
+//     let avg=sum/cnt;
+//     console.log(avg);
+// }
+
+// const numberArray=[42,13,58,65,81,96,7,105];
+
+// oddAvg(numberArray);
+
+
+
+
+
+// function cheakdublicate(arr)
+// {
+//     let arr2=[];
+    
+//     for(let i of arr)
+//     {
+//        if(!arr2.includes(i) )
+//        {
+//         arr2.push(i);
+//        }
+        
+//     }
+//     console.log(arr);
+//     console.log(arr2);
+
+
+   
+// }
+
+// const numberArray=[44,1,3,22,4,23,4,7,1];
+
+
+// cheakdublicate(numberArray);
+// const str=["alamin","rana","alamin","Rakib","rana"];
+// cheakdublicate(str);
+
+
+// const numberArray=[44,1,3,22,4,23,4,7,1];
+
+// console.log(Math.min(...numberArray));
+// console.log(Math.max(...numberArray));
+
+
+
+// console.log(Math.floor(2.89));
+// console.log(Math.ceil(2.45));
+// console.log(Math.abs(-5));
+// console.log(Math.round(2.44));
+// console.log(Math.round(2.74));
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.round(Math.random()*10));
+
